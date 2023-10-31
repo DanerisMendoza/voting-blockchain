71f581de1dd4bb0f60939700c20fb7085f1d287f
Daneris Mendoza <danerismendoza096@gmail.com>
2023-10-31 08:21:37 +0800

    erd column added

--- backend/database/migrations/2023_10_31_000710_create_users_table.php
+++ backend/database/migrations/2023_10_31_000710_create_users_table.php
@@ -13,7 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('username');
+            $table->string('password');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('gender');
+            $table->string('profile_pic_path');
+            $table->string('address');
+            $table->integer('age');
         });
     }
 
--- backend/database/migrations/2023_10_31_000720_create_candidates_table.php
+++ backend/database/migrations/2023_10_31_000720_create_candidates_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('candidates', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->integer('user_id');
+            $table->integer('posistion_id');
         });
     }
 
