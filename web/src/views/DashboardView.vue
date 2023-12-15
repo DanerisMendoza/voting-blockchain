3d2c5bd0015dab2e4684d9cb2cc79e76fe42bca9
dyiroooo <20200097m.ibia.jiroallen.bscs@gmail.com>
2023-12-16 00:32:03 +0800

    [Adjusted the Seeder] Candidate Seeder and Election Seeder was adjusted and Dashboard of Admin and Users are now Working

--- web/src/views/DashboardView.vue
+++ web/src/views/DashboardView.vue
@@ -12,7 +12,18 @@
                         <v-btn color="primary" @click="openVDialog" tile small>Set Voting Schedules</v-btn>
                     </v-card-title>
                     <v-divider></v-divider>
-                    <v-card-text></v-card-text>
+                    <v-card-text v-if="GET_EL_STATUS == 'finished'">
+
+                    </v-card-text>
+                    <v-card-text v-else>
+                        <p>Election Status: <strong style="text-transform: uppercase;">{{ GET_EL_STATUS }}</strong></p>
+                        <div>
+                            <p>Filing Start: <strong>{{ formattedDate(GET_ELECTION.start_filing_date) }}</strong></p>
+                            <p>Filing End: <strong>{{ formattedDate(GET_ELECTION.end_filing_date) }}</strong></p>
+                            <p>Election Start: <strong>{{ formattedDate(GET_ELECTION.start_voting_date) }}</strong></p>
+                            <p>Election End: <strong>{{ formattedDate(GET_ELECTION.end_voting_date) }}</strong></p>
+                        </div>
+                    </v-card-text>
                 </v-card>
                 <ScheduleDialogVue />
             </v-container>
@@ -24,12 +35,21 @@
 import ScheduleDialogVue from "@/components/Dialogs/ScheduleDialog.vue";
 import { mapGetters } from "vuex";
 import AdminNav from "@/components/Navbars/AdminNav.vue"
+import moment from "moment";
 export default {
     data() {
         return {
         }
     },
     methods: {
+        async main() {
+            await this.$store.dispatch("GetElectionStatus");
+            await this.$store.dispatch("IsVoted");
+            await this.$store.dispatch("GetActiveElection");
+        },
+        formattedDate(data) {
+            return moment(data).format("MMMM D, YYYY - hh:mm A");
+        },
         openFDialog() {
             this.$store.commit("SET_FILE_DIALOG", true);
         },
@@ -43,8 +63,13 @@ export default {
     },
     computed: {
         ...mapGetters([
-            'GET_SCHED_DIALOG'
+            'GET_SCHED_DIALOG',
+            'GET_EL_STATUS',
+            'GET_ELECTION'
         ])
+    },
+    mounted() {
+        this.main();
     }
 }
 </script>
